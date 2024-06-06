733d2393bd2d5a86a6fd7851840310f59fc814a8
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-06-06 11:53:49 -0400

    cambios varios

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cristianrolendiovillavicencio/gonzalezroofing',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5e0078424b2ebfe2b5a6b46126bf7dbedf86d3e6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cristianrolendiovillavicencio/gonzalezroofing' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5e0078424b2ebfe2b5a6b46126bf7dbedf86d3e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
